4ea21e90037cbf8df6764624670e1f30b4c733de
Karim Jalil <karim.jalil@accuv.com>
2018-12-21 15:05:09 -0600

    Finishing Second Session

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -11,19 +11,36 @@ namespace FirstProject
         static void Main(string[] args)
         {
             // The classig Hello World
-            Console.WriteLine("Hello World");
+            Console.WriteLine("1. Hello World");
 
 
             // ***** Implicit Type Conversion *****
             byte b = 1;
             int ib = b;
-            Console.WriteLine("IMPLICIT Conversion of Byte (" + b + ") into Int (" + ib + ")");
+            Console.WriteLine("2. IMPLICIT Conversion of Byte (" + b + ") into Int (" + ib + ")");
 
             // ***** Explicit Type Conversion *****
             string s = "1";
             int i = Convert.ToInt32(s);
             int j = int.Parse(s);
-            Console.WriteLine("EXPLICIT Conversion of String (" + s + ") using Convert.ToInt32 (" + i + ") and int.Parse (" + j + ")");
+            Console.WriteLine("3. EXPLICIT Conversion of String (" + s + ") using Convert.ToInt32 (" + i + ") and int.Parse (" + j + ")");
+
+            // ***** Explicit Type Conversion Failing, adding Try-Catch *****
+            try
+            {
+                var number = "1234";
+                byte secondB = Convert.ToByte(number);
+                Console.WriteLine(secondB);
+            }
+            catch (Exception)
+            {
+                //throw;
+                Console.WriteLine("4. There was an EXCEPTION catched while converting that Number to Byte. Asshole!!!");
+            }
+
+           
+
+            
     }
     }
 }

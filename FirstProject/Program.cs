d8ed69210116a72689f899222ef16cb188569909
Karim Jalil <karim.jalil@accuv.com>
2018-12-21 14:40:30 -0600

    Second Lesson

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -10,7 +10,20 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
+            // The classig Hello World
             Console.WriteLine("Hello World");
-        }
+
+
+            // ***** Implicit Type Conversion *****
+            byte b = 1;
+            int ib = b;
+            Console.WriteLine("IMPLICIT Conversion of Byte (" + b + ") into Int (" + ib + ")");
+
+            // ***** Explicit Type Conversion *****
+            string s = "1";
+            int i = Convert.ToInt32(s);
+            int j = int.Parse(s);
+            Console.WriteLine("EXPLICIT Conversion of String (" + s + ") using Convert.ToInt32 (" + i + ") and int.Parse (" + j + ")");
+    }
     }
 }

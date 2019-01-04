c9bf44bf02b3a146b7cab405d0b4f2fb95ab9ad1
Karim Jalil <karim.jalil@accuv.com>
2019-01-04 15:29:34 -0600

    Classes, Arrays and Strings

---
+++ FirstProject/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hello my name is " + FirstName + " " + LastName);
+        }
+    }
+}
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace FirstProject
 {
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The classig Hello World
+            // The classic Hello World
             Console.WriteLine("1. Hello World");
 
 
@@ -35,7 +37,7 @@ namespace FirstProject
             catch (Exception)
             {
                 //throw;
-                Console.WriteLine("4. There was an EXCEPTION catched while converting that Number to Byte. Asshole!!!");
+                Console.WriteLine("4. There was an EXCEPTION caught while converting that Number to Byte. Asshole!!!");
             }
 
             // ***** OPERATORS *****
@@ -46,6 +48,55 @@ namespace FirstProject
             int opThree = ++opOne;
             Console.WriteLine("Prefix Increment (b = ++a: opOne = " + opOne + " opThree = " + opThree);
 
+
+            Person client = new Person();
+            client.FirstName = "John";
+            client.LastName = "Verga";
+            client.Introduce();
+
+            var newClient = new Person();
+            newClient.FirstName = "Jeremy";
+            newClient.LastName = "Piturri";
+            newClient.Introduce();
+
+            var calc = new Calculator();
+            var result = calc.Add(5, 6);
+            Console.WriteLine(result);
+
+            // ***** ARRAYS *****
+            int[] coordinates = new int[3];
+            coordinates[0] = 1;
+            coordinates[1] = 2;
+            coordinates[2] = 3;
+
+            // Easier way to declare / initialize arrays
+            int[] easyArray = new int[3] {1,2,3};
+            // Even easier
+            var easiestArray = new int[3] { 1, 2, 3 };
+
+            Console.WriteLine("Arrays");
+            Console.WriteLine(easiestArray[0]);
+            Console.WriteLine(easiestArray[1]);
+            Console.WriteLine(easiestArray[2]);
+
+            Console.WriteLine(string.Join(",",easyArray));
+
+            // ***** STRINGS *****
+            // Strings are immutable, so once you assign them a value, you can't change it
+            string firstString = "First Name";
+            string lastString = "Last Name";
+            string fullName = string.Format("{0} {1}", firstString, lastString);
+            Console.WriteLine("Strings");
+
+            Console.WriteLine(fullName);
+
+            // Using @ prior to a string allows you to avoid using escape characters like \n or \\ 
+            string verb = @"I can enter txt
+in different lines
+and use paths much simpler
+c:\users\bo
+c:\users\io";
+            Console.WriteLine(verb);
         }
     }
 }
